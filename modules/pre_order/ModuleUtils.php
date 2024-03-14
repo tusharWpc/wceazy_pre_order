4953e26a7da1df07535cf32476c3ad09974fff1e
tusharWpc <nftusharwpcommerz@gmail.com>
2024-03-14 14:26:36 +0600

    problem on mail

--- modules/pre_order/ModuleUtils.php
+++ modules/pre_order/ModuleUtils.php
@@ -373,8 +373,7 @@ if (!class_exists('WcEazyPreOrderUtils')) {
             return $price_html;
         }
 
-        ///////////////////////////////
-        // Callback function to display pre-order information in the order details pagepublic function
+
 
         // Schedule a task to update product availability and send email notifications when pre-order period ends
         public function schedule_preorder_availability_update()
@@ -624,9 +623,6 @@ if (!class_exists('WcEazyPreOrderUtils')) {
         // Function to filter orders by pre-order products
         public function filter_orders_by_preorder_products($args)
         {
-            // echo "<pre>";
-            // printf($args);
-            // echo "</pre>";
             // Check if the 'orders' property exists and is not null
             if (isset($args['orders'])) {
                 // Convert the 'orders' property to an array if it's an object
